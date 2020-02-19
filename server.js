1102202fdef59212d4e5431674d6c596ba446c18
Burhan <bharmal.burhan@hotmail.com>
2020-02-19 16:25:48 +0530

    minor bug fix on server

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ app.use(
   }),
 );
 
+app.use(express.static('public'))
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-app.use(express.static('public'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
