dc9f21825b49b05ddd49adbe0d8a5802ad819c05
Burhan <bharmal.burhan@hotmail.com>
2020-02-19 16:16:46 +0530

    Finished React

---
+++ public/precache-manifest.97e1dd9c4f685eb9b1c1db8b92255488.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4f949d84671146185e190f848ad1a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d9275ce7758f3bc8a2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "31d5eaecbc57d2186adc",
+    "url": "/static/js/2.eb3cfcea.chunk.js"
+  },
+  {
+    "revision": "a0c18f8786874a74d6fd4d46260562f6",
+    "url": "/static/js/2.eb3cfcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d9275ce7758f3bc8a2",
+    "url": "/static/js/main.590aee39.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
